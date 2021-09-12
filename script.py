fa6fb7b244d0fab8ea55d7cd1e85101495e4ea8e
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2021-09-12 12:13:52 +0200

    Fix message

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 def main():
-    logging.info("You are now working in the directory: " + os.getcwd())
+    logging.info("The script is now working in the directory: " + os.getcwd())
 
 if __name__ == "__main__":
     print("Hello world!")
